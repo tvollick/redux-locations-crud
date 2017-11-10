3bd3a3c6e85e2939ca048f185845bd2d801d5395
Ty Vollick <tvollick@wearerivet.com>
2017-11-10 17:03:48 -0600

    changed locations into cards

--- src/components/locations/LocationsList.js
+++ src/components/locations/LocationsList.js
@@ -4,23 +4,32 @@ import { Link } from 'react-router';
 const LocationsList = (props) => {
   const listItems = props.locations.map((location, index) => {
     return (
-      <li key={index}>
-        <div className="locationsListItem">
-          <h4>{location.name ? location.name : "No Name"}</h4>
-          <div className="btn-group">
-            <Link
-              className="btn btn-default"
-              to={`/location/${location.id}`}
-              >
-              Edit
-            </Link>             <button
-              className="btn btn-danger"
-              onClick={() => {
-                props.handleDeleteLocation(location.id);
-              }}
-              >
-              Delete
-            </button>
+      <li key={index} className="col-sm-4">
+        <div className="locationsListItem panel panel-default">
+          <div className="panel-body">
+            <h4>{location.name ? location.name : "No Name"}</h4>
+            <div>
+              {location.addr1 && <div>{location.addr1}</div>}
+              {location.addr2 && <div>{location.addr2}</div>}
+              {location.city && <div>{location.city}</div>}
+              {location.state && <div>{location.state}</div>}
+              {location.zip && <div>{location.zip}</div>}
+            </div>
+            <div className="btn-group">
+              <Link
+                className="btn btn-default"
+                to={`/location/${location.id}`}
+                >
+                Edit
+              </Link>             <button
+                className="btn btn-danger"
+                onClick={() => {
+                  props.handleDeleteLocation(location.id);
+                }}
+                >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
       </li>
@@ -28,9 +37,9 @@ const LocationsList = (props) => {
   });
 
   const list = (
-    <ul className="locationsList">
+    <div className="locationsList row">
       {listItems}
-    </ul>
+    </div>
   );
 
   const noLocations = (
@@ -45,7 +54,7 @@ const LocationsList = (props) => {
   console.log()
 
   return(
-    <div className="locationsListWrap">
+    <div className="locationsListWrap container">
       {props.locations.length ? list : noLocations }
     </div>
   );
